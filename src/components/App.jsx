33c8f42fc6cc8bb61ed1d707f47da35ba66f9671
Alexander Schlieden <alex.schlieden@gmail.com>
2023-06-04 23:05:59 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '99vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      React homework template 2
     </div>
   );
 };
